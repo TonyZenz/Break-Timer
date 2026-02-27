e59ace2a894373192e04e9c07727634eef35fb79
Tony Zenz <zenzer4@gmail.com>
2026-02-26 18:34:40 -0600

    Bugfixes and added flashing screen

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,10 +1,9 @@
-﻿using BreakTimer.Custom;
-
-namespace BreakTimer;
+﻿namespace BreakTimer;
 
 public partial class MainPage : ContentPage
 {
     private bool isRunning = false;
+    private bool isFlashing = false;
     int intSec = 0;
     int intMin = 0;
     
@@ -14,48 +13,101 @@ public partial class MainPage : ContentPage
         Title = "Break Timer";
     }
     
-    public void Tick(int min)
+    private void Tick()
     {
-        intMin = min;
         intSec++;
         if (intSec == 60)
         {
             intSec = 0;
             intMin--;
         }
+        lblDisplay.Text = $"{intMin} minutes left";
+    }
 
-        lblDisplay.Text = $"{min} minutes left";
-        
+    private void StartFlashing()
+    {
+        isFlashing = true;
+        bool flashOn = false;
+
+        Application.Current.Dispatcher.StartTimer(TimeSpan.FromSeconds(1), () =>
+        {
+            flashOn = !flashOn;
+            ftmMain.BackgroundColor = flashOn ? Colors.Red : Colors.White;
+            return isFlashing;
+        });
     }
 
     private void BtnTakeFive_OnClicked(object sender, EventArgs e)
     {
+        // Initialize variables
         isRunning = true;
-        int intMin = 1;
+        isFlashing = false;
+        intMin = 1;
+        intSec = 0;
         
         Application.Current.Dispatcher.StartTimer(TimeSpan.FromSeconds(1), () =>
         {
-            if (isRunning)
-            {
-                Tick(intMin);
-            }
-
+            
             if (intMin == 0 && intSec == 0)
             {
                 isRunning = false;
                 lblDisplay.Text = "Time's up! Back to Work!";
+                StartFlashing();
             }
-            
+            if (isRunning)
+            {
+                Tick();
+            }
             return (isRunning);
         } );
 }
 
     private void BtnTakeTen_OnClicked(object sender, EventArgs e)
     {
+        isRunning = true;
+        isFlashing = false;
+        intMin = 10;
+        intSec = 0;
+        
+        Application.Current.Dispatcher.StartTimer(TimeSpan.FromSeconds(1), () =>
+        {
+            
+            if (intMin == 0 && intSec == 0)
+            {
+                isRunning = false;
+                lblDisplay.Text = "Time's up! Back to Work!";
+                StartFlashing();
+            }
+            if (isRunning)
+            {
+                Tick();
+            }
+            return (isRunning);
+        } );
     }
 
     private void BtnTakeFifteen_OnClicked(object sender, EventArgs e)
     {
+        isRunning = true;
+        isFlashing = false;
+        intMin = 15;
+        intSec = 0;
+        
+        Application.Current.Dispatcher.StartTimer(TimeSpan.FromSeconds(1), () =>
+        {
+            
+            if (intMin == 0 && intSec == 0)
+            {
+                isRunning = false;
+                lblDisplay.Text = "Time's up! Back to Work!";
+                StartFlashing();
+            }
+            if (isRunning)
+            {
+                Tick();
+            }
+            return (isRunning);
+        } );
         
     }
 }
